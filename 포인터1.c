c454fc93d41000eb57d6842b4b201e1ea71bed0b
정우재 <80890472+woojae05@users.noreply.github.com>
2021-08-19 18:01:21 +0900

    Create 포인터1.c

---
+++ 포인터1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+	static int i = 7, * p, ** pp;
+	p = &i;
+	pp = &p;
+
+	printf("p\'s Address = %p\n", p);
+	printf("pp\'s Address =%p", pp);
+	(*p)++;
+	printf("i=%d \t", i);
+	printf("*p=%d\t", *p);
+	printf("**pp=%d \n", **pp);
+
+	(**pp)++;
+	printf("i=%d \t", i);
+	printf("p=%d\t", *p);
+	printf("**p=%d \t", **pp);
+	return 0;
+}
